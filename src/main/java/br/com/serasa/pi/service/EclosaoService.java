a9898ee3ce5547c2085300796ecf39d6012d540e
VivianeMayumi <vmogusko@email>
2022-06-11 16:28:22 -0300

    Atualizacao do metodo update EclosaoService.

--- src/main/java/br/com/serasa/pi/service/EclosaoService.java
+++ src/main/java/br/com/serasa/pi/service/EclosaoService.java
@@ -11,32 +11,43 @@ import br.com.serasa.pi.repository.EclosaoRepository;
 
 @Service
 public class EclosaoService {
-	
+
 	@Autowired
 	EclosaoRepository repository;
-	
-	public Eclosao insert (Eclosao Eclosao) {
+
+	public Eclosao insert(Eclosao Eclosao) {
 		return repository.save(Eclosao);
 	}
-	
-	public List<Eclosao> findAll () {
+
+	public List<Eclosao> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Eclosao findById(Integer idEclosao) {
 		Optional<Eclosao> obj = repository.findById(idEclosao);
-				return obj.get();
+		return obj.get();
 	}
-	
+
 	public void delete(Integer idEclosao) {
-		repository.deleteById(idEclosao);		
+		repository.deleteById(idEclosao);
 	}
-	
-	
-	public Eclosao update(Integer idEclosao, Eclosao Eclosao) { 
-		  Eclosao entity = findById(idEclosao); 
-		  entity.setEspecie(Eclosao.getEspecie()); 
-		  entity.setComunidade(Eclosao.getComunidade()); 
-		  return repository.save(entity); 
-		 }		
+
+	public Eclosao update(Integer idEclosao, Eclosao Eclosao) {
+		Eclosao entity = findById(idEclosao);
+		entity.setDataViagem(Eclosao.getDataViagem());
+		entity.setEstadoUF(Eclosao.getEstadoUF());
+		entity.setMunicipio(Eclosao.getMunicipio());
+		entity.setComunidade(Eclosao.getComunidade());
+		entity.setNumeroCova(Eclosao.getNumeroCova());
+		entity.setDataNascimento(Eclosao.getDataNascimento());
+		entity.setEspecie(Eclosao.getEspecie());
+		entity.setQuantidadeFilhoteVivo(Eclosao.getQuantidadeFilhoteVivo());
+		entity.setQuantidadeOvoInviavel(Eclosao.getQuantidadeOvoInviavel());
+		entity.setQuantidadeOvoInfertil(Eclosao.getQuantidadeOvoInfertil());
+		entity.setQuantidadeFilhoteMortoFormiga(Eclosao.getQuantidadeFilhoteMortoFormiga());
+		entity.setQuantidadeFilhoteMortoBicheira(Eclosao.getQuantidadeFilhoteMortoBicheira());
+		entity.setQuantidadeFilhoteMortoOutros(Eclosao.getQuantidadeFilhoteMortoOutros());
+
+		return repository.save(entity);
+	}
 }
