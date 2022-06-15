1b5891dd211196e7d2e9f1d1e380e875ffbb5f06
Viviane Ogusko <vmogusko@gmail.com>
2022-06-15 14:15:50 -0300

    Finalização - Ajustes Entity para VO - Coleta

--- src/main/java/br/com/serasa/pi/service/ColetaService.java
+++ src/main/java/br/com/serasa/pi/service/ColetaService.java
@@ -29,23 +29,25 @@ public class ColetaService {
 
 	public List<ColetaVO> findAll() {
 		List<Coleta> allColetas = repository.findAll();
-		
+
 		List<ColetaVO> retorno = new ArrayList<>();
-		if(allColetas != null && !allColetas.isEmpty()) {
-			
-			// Forma com 'for' tradicional...
-			for(int i = 0; i < allColetas.size(); i++) {
-				Coleta coleta = allColetas.get(i);
-				ColetaVO coletaVO = coletaMapper.coletaToColetaVO(coleta);
-				retorno.add(coletaVO);
-			}
-			
+		if (allColetas != null && !allColetas.isEmpty()) {
+
+			retorno = coletaMapper.listColetaToListColetaVO(allColetas);
+
+//			Forma com 'for' tradicional...
+//			for(int i = 0; i < allColetas.size(); i++) {
+//				Coleta coleta = allColetas.get(i);
+//				ColetaVO coletaVO = coletaMapper.coletaToColetaVO(coleta);
+//				retorno.add(coletaVO);
+//			}
+
 //			Foreach...
 //			for(Coleta coleta : allColetas) {
 //				ColetaVO coletaVO = coletaMapper.coletaToColetaVO(coleta);
 //				retorno.add(coletaVO);
 //			}
-			
+
 //			Utilizando as Streams....
 //			retorno = allColetas.stream().map(
 //				coleta -> {
@@ -56,34 +58,94 @@ public class ColetaService {
 		return retorno;
 	}
 
-	public Coleta findById(Integer idColeta) {
-		Optional<Coleta> obj = repository.findById(idColeta);
-		return obj.get();
+	public ColetaVO findById(Integer idColeta) {
+		Optional<Coleta> optionalColeta = repository.findById(idColeta);
+		ColetaVO retorno = null;
+		if (optionalColeta.isPresent()) {
+			retorno = coletaMapper.coletaToColetaVO(optionalColeta.get());
+		}
+		return retorno;
 	}
 
 	public void delete(Integer idColeta) {
 		repository.deleteById(idColeta);
 	}
 
-	public Coleta update(Integer idColeta, Coleta Coleta) {
-		Coleta entity = findById(idColeta);
-		entity.setDataViagem(Coleta.getDataViagem());
-		entity.setEstadoUF(Coleta.getEstadoUF());
-		entity.setMunicipio(Coleta.getMunicipio());
-		entity.setComunidade(Coleta.getComunidade());
-		entity.setDataColeta(Coleta.getDataColeta());
-		entity.setNomePraiaTabuleiro(Coleta.getNomePraiaTabuleiro());
-		entity.setNumeroCova(Coleta.getNumeroCova());
-		entity.setQuantidadeOvos(Coleta.getQuantidadeOvos());
-		entity.setEspecie(Coleta.getEspecie());
-		entity.setDistanciaAgua(Coleta.getDistanciaAgua());
-		entity.setDistanciaVegetacao(Coleta.getDistanciaVegetacao());
-		entity.setProfundidadePrimeiroOvo(Coleta.getProfundidadePrimeiroOvo());
-		entity.setProfundidadeTotal(Coleta.getProfundidadeTotal());
-		entity.setLarguraNinho(Coleta.getLarguraNinho());
-		entity.setLarguraPata(Coleta.getLarguraPata());
-		entity.setLarguraEntrePatas(Coleta.getLarguraEntrePatas());
+	public ColetaVO update(Integer idColeta, ColetaVO coletaVoAtualizacao) {
+		Optional<Coleta> optionalColeta = repository.findById(idColeta);
+		if (optionalColeta.isPresent()) {
+			Coleta coletaEncontrada = optionalColeta.get();
+			Coleta coletaAtualizacao = coletaMapper.coletaVOToColeta(coletaVoAtualizacao);
 
-		return repository.save(entity);
+			if (coletaAtualizacao.getDataViagem() != null) {
+				coletaEncontrada.setDataViagem(coletaAtualizacao.getDataViagem());
+			}
+
+			if (coletaAtualizacao.getEstadoUF() != null) {
+				coletaEncontrada.setEstadoUF(coletaAtualizacao.getEstadoUF());
+			}
+
+			if (coletaAtualizacao.getMunicipio() != null) {
+				coletaEncontrada.setMunicipio(coletaAtualizacao.getMunicipio());
+			}
+
+			if (coletaAtualizacao.getComunidade() != null) {
+				coletaEncontrada.setComunidade(coletaAtualizacao.getComunidade());
+			}
+
+			if (coletaAtualizacao.getDataColeta() != null) {
+				coletaEncontrada.setDataColeta(coletaAtualizacao.getDataColeta());
+			}
+
+			if (coletaAtualizacao.getNomePraiaTabuleiro() != null) {
+				coletaEncontrada.setNomePraiaTabuleiro(coletaAtualizacao.getNomePraiaTabuleiro());
+			}
+
+			if (coletaAtualizacao.getNumeroCova() != null) {
+				coletaEncontrada.setNumeroCova(coletaAtualizacao.getNumeroCova());
+			}
+
+			if (coletaAtualizacao.getQuantidadeOvos() != null) {
+				coletaEncontrada.setQuantidadeOvos(coletaAtualizacao.getQuantidadeOvos());
+			}
+
+			if (coletaAtualizacao.getEspecie() != null) {
+				coletaEncontrada.setEspecie(coletaAtualizacao.getEspecie());
+			}
+
+			if (coletaAtualizacao.getDistanciaAgua() != null) {
+				coletaEncontrada.setDistanciaAgua(coletaAtualizacao.getDistanciaAgua());
+			}
+
+			if (coletaAtualizacao.getDistanciaVegetacao() != null) {
+				coletaEncontrada.setDistanciaVegetacao(coletaAtualizacao.getDistanciaVegetacao());
+			}
+
+			if (coletaAtualizacao.getProfundidadePrimeiroOvo() != null) {
+				coletaEncontrada.setProfundidadePrimeiroOvo(coletaAtualizacao.getProfundidadePrimeiroOvo());
+			}
+
+			if (coletaAtualizacao.getProfundidadeTotal() != null) {
+				coletaEncontrada.setProfundidadeTotal(coletaAtualizacao.getProfundidadeTotal());
+			}
+
+			if (coletaAtualizacao.getLarguraNinho() != null) {
+				coletaEncontrada.setLarguraNinho(coletaAtualizacao.getLarguraNinho());
+			}
+
+			if (coletaAtualizacao.getLarguraPata() != null) {
+				coletaEncontrada.setLarguraPata(coletaAtualizacao.getLarguraPata());
+			}
+
+			if (coletaAtualizacao.getLarguraEntrePatas() != null) {
+				coletaEncontrada.setLarguraEntrePatas(coletaAtualizacao.getLarguraEntrePatas());
+			}
+
+			Coleta coletaAtualizada = repository.save(coletaEncontrada);
+			return coletaMapper.coletaToColetaVO(coletaAtualizada);
+		} else {
+			// Lançar exceção
+			throw new RuntimeException("Coleta não encontrada");
+		}
 	}
 }
