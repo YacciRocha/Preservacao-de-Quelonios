d434bf42f55fb2989635b0e31eb482b9e029e04f
VivianeMayumi <vmogusko@gmail.com>
2022-06-28 00:47:08 -0300

    Ajustes de permissoes

--- src/main/java/br/com/serasa/pi/enums/TipoUsuarioEnum.java
+++ src/main/java/br/com/serasa/pi/enums/TipoUsuarioEnum.java
@@ -1,7 +1,15 @@
 package br.com.serasa.pi.enums;
 
 public enum TipoUsuarioEnum {
-	ADMIN,
-	COORDENADOR,
-	VOLUNTARIO
+	ADMIN(1), COORDENADOR(2), VOLUNTARIO(3);
+
+	public long codigoTipoUsuario;
+
+	TipoUsuarioEnum(long codigoTipoUsuario) {
+		this.codigoTipoUsuario = codigoTipoUsuario;
+	}
+	
+	public long getCodigoTipoUsuario() {
+		return this.codigoTipoUsuario;
+	}
 }
--- src/main/java/br/com/serasa/pi/service/UsuarioService.java
+++ src/main/java/br/com/serasa/pi/service/UsuarioService.java
@@ -3,6 +3,7 @@ package br.com.serasa.pi.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -13,32 +14,43 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.serasa.pi.common.UsuarioVO;
+import br.com.serasa.pi.domain.entity.PermissaoEntity;
 import br.com.serasa.pi.domain.entity.UsuarioEntity;
 import br.com.serasa.pi.exceptions.ResourceNotFoundException;
 import br.com.serasa.pi.mapper.UsuarioMapper;
+import br.com.serasa.pi.repository.PermissaoRepository;
 import br.com.serasa.pi.repository.UsuarioRepository;
 
 @Service
-public class UsuarioService implements UserDetailsService{
+public class UsuarioService implements UserDetailsService {
 
 	@Autowired
-	private UsuarioRepository repository;
+	private UsuarioRepository usuarioRepository;
+
+	@Autowired
+	private PermissaoRepository permissaoRepository;
 
 	@Autowired
 	private UsuarioMapper usuarioMapper;
-	
-	 private BCryptPasswordEncoder  bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
-	public UsuarioVO insert(UsuarioVO usuarioVO) {	
-		usuarioVO.setPassword(bCryptPasswordEncoder.encode(usuarioVO.getPassword()));	
+	private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+	public UsuarioVO insert(UsuarioVO usuarioVO) {
+		usuarioVO.setPassword(bCryptPasswordEncoder.encode(usuarioVO.getPassword()));
 		UsuarioEntity usuarioAInserir = usuarioMapper.usuarioVOToUsuarioEntity(usuarioVO);
-		UsuarioEntity usuarioInserido = repository.save(usuarioAInserir);
-		
+		List<PermissaoEntity> permissoes = new ArrayList<>();
+		Optional<PermissaoEntity> optionalPermissao = permissaoRepository.findById(usuarioVO.getTipoUsuario().getCodigoTipoUsuario());
+		if(optionalPermissao.isPresent()) {
+			permissoes.add(optionalPermissao.get());
+		}
+		usuarioAInserir.setPermissions(permissoes);
+		UsuarioEntity usuarioInserido = usuarioRepository.save(usuarioAInserir);
+
 		return usuarioMapper.usuarioEntityToUsuarioVO(usuarioInserido);
 	}
 
 	public List<UsuarioVO> findAll() {
-		List<UsuarioEntity> allUsuariosEntity = repository.findAll();
+		List<UsuarioEntity> allUsuariosEntity = usuarioRepository.findAll();
 
 		List<UsuarioVO> retorno = new ArrayList<>();
 		if (allUsuariosEntity != null && !allUsuariosEntity.isEmpty()) {
@@ -48,14 +60,14 @@ public class UsuarioService implements UserDetailsService{
 	}
 
 	public UsuarioVO findById(String matricula) {
-		var entity = repository.findById(matricula).orElseThrow(() -> new ResourceNotFoundException(matricula));
+		var entity = usuarioRepository.findById(matricula).orElseThrow(() -> new ResourceNotFoundException(matricula));
 		UsuarioVO retorno = usuarioMapper.usuarioEntityToUsuarioVO(entity);
 		return retorno;
 	}
 
 	public void delete(String matricula) {
 		try {
-			repository.deleteById(matricula);
+			usuarioRepository.deleteById(matricula);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(matricula);
 		}
@@ -63,14 +75,14 @@ public class UsuarioService implements UserDetailsService{
 
 	public UsuarioVO update(String matricula, UsuarioVO usuarioVoAtualizacao) {
 		try {
-			var entity = repository.findById(matricula);
+			var entity = usuarioRepository.findById(matricula);
 
 			UsuarioEntity usuarioEncontrado = entity.get();
 
 			usuarioEncontrado.setNome(usuarioVoAtualizacao.getNome());
 			usuarioEncontrado.setUsername(usuarioVoAtualizacao.getUsername());
 
-			UsuarioEntity usuarioAtualizado = repository.save(usuarioEncontrado);
+			UsuarioEntity usuarioAtualizado = usuarioRepository.save(usuarioEncontrado);
 			return usuarioMapper.usuarioEntityToUsuarioVO(usuarioAtualizado);
 		} catch (NoSuchElementException e) {
 			throw new ResourceNotFoundException(matricula);
@@ -78,12 +90,12 @@ public class UsuarioService implements UserDetailsService{
 	}
 
 	public UsuarioService(UsuarioRepository repository) {
-		this.repository = repository;
+		this.usuarioRepository = repository;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		var usuario = repository.findByUsername(username);
+		var usuario = usuarioRepository.findByUsername(username);
 
 		if (usuario != null) {
 			return usuario;
